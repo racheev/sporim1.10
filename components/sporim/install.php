def7437875323d13bdf2946553ecb661f2fb1673
racheev <racheev@MacBook-Pro-Eduard.local>
2016-01-31 13:35:07 +0300

    Обновим

--- components/sporim/install.php
+++ components/sporim/install.php
@@ -1,48 +1,80 @@
-<?php //004ff
-// IONCUBE ENCODER 8.2 EVALUATION
-// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
-// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+<?php
+// ========================================================================== //
 
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+	function info_component_sporim(){
+        //Описание компонента
+		$_component['title']        = 'Давай поспорим';
+		$_component['description']  = 'Компонент позволяет создавать споры на сайте.';
+		$_component['link']         = 'sporim';
+		$_component['author']       = 'Рачей';
+		$_component['internal']     = '0';
+		$_component['version']      = '1.10.3';
+		$_component['system']    	= '0';
+		return $_component;
+	}
+
+// ========================================================================== //
+
+	function install_component_sporim(){
+		$inCore     = cmsCore::getInstance();       //подключаем ядро
+		$inDB       = cmsDatabase::getInstance();   //подключаем базу данных
+		$inConf     = cmsConfig::getInstance();
+		include($_SERVER['DOCUMENT_ROOT'].'/includes/dbimport.inc.php');
+		dbRunSQL($_SERVER['DOCUMENT_ROOT'].'/components/sporim/install.sql', $inConf->db_prefix);
+		
+		
+		// версия 2 лента и коментарии
+		
+		// проверим если коменты уже к компоненту, если да то не пишем!
+		$sqls = "
+		SELECT id
+		FROM cms_comment_targets 
+		WHERE target='sporim' AND component='sporim'
+		";
+		if (!mysql_num_rows($inDB->query($sqls))){
+		$sql = "INSERT INTO cms_comment_targets (target, component, title)
+				VALUES ('sporim', 'sporim', 'Давай поспорим')";
+		//выполняем запрос
+		$inDB->query($sql);
+		}
+		// проверили наличие компонентов.
+		// удалим лог спора из таблицы
+		$sql = "DELETE FROM cms_actions WHERE component = 'sporim'";
+		//выполняем запрос
+		$inDB->query($sql);
+		// удалили
+		// очистим лог при переинсталяции ?? потом если желание будет
+		
+		// удалили
+
+		// добавление в ленту активности задел на следующую версию
+		if(!cmsActions::getAction('sporim')){
+			cmsActions::registerAction('sporim',
+				array(
+					'name'=>'add_sporim',
+					'title'=>'Давай поспорим',
+					'message'=>'Предлагает новую тему для спора: %s, |'
+				)
+			);
+		}
+		
+		//
+		
+		// лента активности
+		return true;
+
+	}
+// ========================================================================== //
+
+	function upgrade_component_sporim(){
+		$inCore     = cmsCore::getInstance();       //подключаем ядро
+		$inDB       = cmsDatabase::getInstance();   //подключаем базу данных
+		$inConf     = cmsConfig::getInstance();
+		$cfg        = $inCore->loadComponentConfig('sporim');
+		include($_SERVER['DOCUMENT_ROOT'].'/includes/dbimport.inc.php');
+		dbRunSQL($_SERVER['DOCUMENT_ROOT'].'/components/sporim/upgrade.sql', $inConf->db_prefix);
+		return true;
+	}
+
+// ========================================================================== //
 ?>
-HR+cPyYJCZWgVOS/9o6d6qIFcAM15VsX50z8lT0aipdjpBAsBIUNiUc/T0avgMvdXqgBXsIIbuVp
-0fZS74qeHn7gleULPZX/WYnG4i2VljmsGqntQneYsn7yPgton4IJHTbVGFgFBlwC8F6lMyddqauG
-gRQ+0zHbcaKEttIgLffmmGJl0Jf7Rl9BbkWCHJqYPx8mssXhsvyurUe7YZ234xlJ6BpFJlBdZ0q8
-TjhkszkVSPuZ4DmNUGwBiIfhXP46kOYJPqBYYwc7owq1OLdt6lAsmYsSnZP5qajc6QBUZRrpXh0G
-QQpQ38XrnPUlkQ7evma14LDz9Oo6lhLCd/Znw5pXEUdVb1IXqSR+iCAEPfjer90pf8TvIl0oZbEP
-OPhd5px9V0+Al75Q8azfLeITY6DHBZYzbLaIwtW33xFImLVLhp5aqmboXGsV9hNqAoQ9ETELoq2O
-B84qhe1mY+o+ObwMhxVeK9i7zDKIWB5S/qoZCDFNJSEXcsOuXbLBi4sPSHrS7Suj+PSkk5Hebl7Y
-o7GnVYrtSxW7Bd+R7lh6H4zgYykm2+090xRYi0Yxjyx9U0vNQauEpv/KXZrGHe1/+5FdqhCugGic
-oS1+Ocqn++xOW5WEEgxYRpbQSocSsjPN/zAEk2/8n7mOmHWkw7FJ8Dta+9sPzKZ2iTYHpyPr+4BI
-fj5l0myEMgBk8hMSPz2Yi/cIra//4tmA6/3S3VDoLMuCBdwHtOZOGY7gvTIE5/I1VCTl7HEscIHl
-VUJ0CyJKJSjHGFpUw7TD5+YadcZ3vXtHcJQ4U9acwnNrbLtXkyd6tmd5mS9eK6vC8lDo32jzPsQ9
-Ff/fkf5ESZAXoJFpmruPIj4kndEeZpUNT0zCNfJxNWlmPphI2ibOh2rkvaEOBwcvaB2EKP5SXiQN
-4s90It2yc/E6ZXqo7Iq3aGamaHZzNcpM4BdcYatPmYQOviHl1U4ftSaF+i5dIryWYyPJ+2c3EnMe
-j6J75cKqxrPWLocKgFOJOulDMdUrNYS+sHZGlMVgKdQ4LuAP7tdczb8XrnNcAVxyhPc1NEeVGA6P
-JqzYanpkriFwM/YShQKATeRrnJk8+tlrPe2fJhgw2LOWk64JTRcymBrfJjIrMbozpL63Sdh8Ea1J
-a/MHZQhgM/Pyd8juYIg9QXvxhbwCLo+EoM6AZtREeGuIOd9oW4Wgn1egh3FcaKXC4/u8uJLyLT2p
-Mf/vS86PARTXaTEfCFji0/e54ycaOhYOMoDO/JRKCKlREj4+wtWzmd6Uc0BRtzDklBjVkKwUtI/F
-QigH1HhM296p/T2lKyDbMeu7Hr9iD0f6QVBe9V/C5lkrb5ysT2BwovCELNr4dMnd2jdUoasBM8Ed
-/U5WeDq5Gao00sDwFH4Q7ru3XqZcXPySrpAUUPbUxFBSDsZT3NqtaxEHHOeCQMYSWvyRaCVuTvfn
-+NqcIAZSLCO2zlUVTie0F+JNLzZx74VvuQTxtwEnB+baUuHjx0QsmJG3XIwUGj868jmeyYGaZuO4
-bb/dI4eNDxgj9OUWyRtC5m3a8ZD7SFV3ltLSZ2DxGrT/QswvsbR8mVdltc7ZS0v6Y9klJpUqu9f2
-Gkm99VEUqYgnONHQbOmKWUtHkl9c+TI7rIH8hcpJ2XyovoEr8WVd+MbuRGVs56bs9EVwq2n2PSH4
-/wpfs2qXMqJLvBa1dMeNAfJqiVAxavYWMbavM3vDYW+hvDVDLMH4J1Xxfi9As2U4E/LRJNhipkWT
-Nhu/c7elVyLuohbdCphFipj1WfdEoOFkLEZmE2TRdJMSS7Mi+SmQoYheypXARjFpy7EsSb5Zx5NX
-yZW7GvSaN+LCmzQdXjOMRqrJ2KjDc2U4M5RgFbpknWDhofnSnQYJX+8Lvx4KTCWpwtOOTTXQyqr5
-WfHeMaB8051XP1EhWF2Sx4zp2kIgsigxv2ok7Gf2DZGJ5PzmL72Ft4NLb2oscsiNrFV580F2Rpcc
-B5u8ULq6eg12qeo9TfRz2LCBO6KqBiyx8TNutbd/Zq1no7AvLZkKWsvrDNR0BucmnsYiV24ceaBX
-WkUeyPwX6FUcpDgKYmy7ae1Qi0JArQyYIWoKxzDY61hftxvT05Ncw1WPjhin5OSReG6RdfvRQccT
-hVZyeZVTnFOT5Jg+q/03ATnxGV+dxQcp7Khj11rMJ35v7ngxDDdXRZ6CVqiACYEJkXev5ZDBrh5i
-46KSd+6MdxJWYGzdk6W+r44oHiu1dOMdfXrpe6TGPVW4KiEb7eOj9M4jf6M0146FEJbKCBpMrU30
-veREfaUqzvr0PGWYegG+RnpEpkr+azu5qLw87kupFoI+6C/AwBBdqUcJiwTw3G02SWurDEPoUTCo
-RSI1gOn/faoqGJDRor70Cuj6HZA3OPuDtvChuENIqQqXou9pa3ZUhqhypsdzubeFpnhB2+jMH9uq
-qZzIkA/PojpEHG/RySWBKpYoaq9XYsp33pS+jKAQFMM0V4iLO2s1RSXvHDx40BxKDAPJwvAOCuax
-9DaIGfo7bKChdvbNeA2Llv87uR5LXwQ2nokyzwLHciFIcvedN4ghB2XrZfluVkOkC/K1qR3ypsNy
-hfk44U7PdBgGYT2z+NBSdIDqWGt6sV5ta+8wZOPcEjC3KgSo/yPLNeyMs5T1WfX4Pi2ab1vBwwHm
-RdIuIVx7dBCP9ez0sJWB0cBtnvK7XweRhHfGdjtWvNKE3Y1VgwurNJxaTj+IBMriZdOzy4oEDPHH
-2jOu5DJPZD6yg2dOpNZwlInK2RqIYdL+Jr5jkIU10tXRERbx5df8tsvWqCK+9NtTnlB5IiSfy5RU
-ewD612yODt4RAzGrcVa216DZw5IZMWJUECqbvxAqO2uLp5i3jTPaBEAUac5M05GUBk8byWhiOMGu
-lON/8OdOtQA2YzbrDNP/qw/W0SWFGaLHzJgoYxTI5hsXp0AmYDQ6RfUlIvduRnzsxjLTU5k3EW4O
-GNsl2pve9H/uccpEYnNtVD1GYWZz5vz5ka90aCeknj7cNOmh57C6TZ6bUZMPkq/myBEsLhL/oEYj
-64QJWHVjP6eNYSJA5rYqddkrR5dQisPYGIbr/TK/CW6rIXktPW==
